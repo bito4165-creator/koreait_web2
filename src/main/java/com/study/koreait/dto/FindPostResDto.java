c37020fe921799f489053becf51a5e109ddbd5b4
bito4165-creator <bito4165@gmail.com>
2026-05-20 21:42:43 +0900

    crud query

--- src/main/java/com/study/koreait/dto/FindPostResDto.java
+++ src/main/java/com/study/koreait/dto/FindPostResDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Data @Builder
 public class FindPostResDto {
     private String title;
     private String content;
